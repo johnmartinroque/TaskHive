b68c17011d19bb16d52a89faf7ee3f62532e538a
Ikawari-s <lnmanese@student.hau.edu.ph>
2025-09-22 17:34:19 +0800

    invitations and homescreen

--- main/src/components/group/graph/Graph.jsx
+++ main/src/components/group/graph/Graph.jsx
@@ -63,7 +63,7 @@ function Graph({ selectedGroupId }) {
 return (
   <div
     style={{
-      padding: "2rem",
+      padding: "1.5rem",
       background: "rgba(255,255,255, 0.05)",
       borderRadius: "0.75rem",
       height: "100%",
